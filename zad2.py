6e3ee4fe4e872df4239890b5dbeb53ebce3b71e8
taararara <43010424+taararara@users.noreply.github.com>
2024-04-30 09:04:20 +0200

    Delete zad2.py, zad3.py

--- zad2.py
+++
@@ -1,6 +0,0 @@
-from itertools import product
-list1 = input("podaj litere pierwsza:"), input("podaj litere druga:")
-list2 = input("podaj litere trzecia:"), input("podaj litere czwarta:")
-
-kombinacja = list(product(list1, list2))
-print(kombinacja)
